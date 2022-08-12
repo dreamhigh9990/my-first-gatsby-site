c51bc86f5ac06c29cab61d43fbde25c92229babc
dreamhigh9990 <dreamhigh9990@gmail.com>
2022-08-13 01:37:24 +1000

    Finished Gatsby Tutorial Part 5

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,12 +1,13 @@
 import * as React from 'react'
 import { graphql } from 'gatsby'
+import { MDXRenderer } from 'gatsby-plugin-mdx'
 import Layout from '../components/layout'
 
 const BlogPage = ({ data }) => {
   return (
     <Layout pageTitle="My Blog Posts">
       {/* <p>My cool posts will go in here</p> */}
-      <ul>
+      {/* <ul>
       {
         data.allFile.nodes.map(node => (
           <li key={node.name}>
@@ -14,19 +15,44 @@ const BlogPage = ({ data }) => {
           </li>
         ))
       }
-      </ul>
+      </ul> */}
+      {
+        data.allMdx.nodes.map((node) => (
+          <article key={node.id}>
+            <h2>{node.frontmatter.title}</h2>
+            <p>Posted: {node.frontmatter.date}</p>
+            <MDXRenderer>
+              {node.body}
+            </MDXRenderer>
+          </article>
+        ))
+      }
     </Layout>
   )
 }
 
+// export const query = graphql`
+//   query {
+//     allFile {
+//       nodes {
+//         name
+//       }
+//     }
+//   }
+// `
 export const query = graphql`
-  query {
-    allFile {
-      nodes {
-        name
+query {
+  allMdx(sort: {fields: frontmatter___date, order: DESC}) {
+    nodes {
+      frontmatter {
+        title
+        date(formatString: "MMMM DD, YYYY")
       }
+      id
+      body
     }
   }
+}
 `
 
 export default BlogPage
